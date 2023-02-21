63f86a0afdf6ec32f263213fbd9e95bd7873c514
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-02-21 17:53:26 +0530

    Day 01 Completed !

---
+++ day-01/band-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+uname= input("What's name of the ciy you grew up in?\n")
+pname= input("What's your pet's name?\n")
+print("Your band name could be " + uname +" " + pname)
---
+++ day-01/exercise1.py
@@ -0,0 +1,3 @@
+print("100- Days of Python Course")
+print("Day 01 Began")
+print("We can print single quotes like this ' '.")
